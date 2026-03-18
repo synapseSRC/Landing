f0a2d94fe9b9bd3478da81f9c56018b4f6c11458
TheRealAshik <mashikahamed0@gmail.com>
2026-03-18 22:31:20 +0600

    add: project file

---
+++ playwright.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    baseURL: 'http://localhost:4321/synapseApp/',
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+  webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:4321/synapseApp/',
+    reuseExistingServer: !process.env.CI,
+  },
+});
